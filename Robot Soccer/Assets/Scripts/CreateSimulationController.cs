1b2e86c6a7099075d0f34799c4fde7e6ba4506c5
Jhon Christian Ambrad <jambrad123@gmail.com>
2018-01-30 19:47:02 +0800

    Implemented new Create Simulation

---
+++ Robot Soccer/Assets/Scripts/CreateSimulationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateSimulationController : MonoBehaviour {
+
+    public Button CreateButton;
+    public InputField TitleInput;
+    public InputField DescInput;
+    public Slider PlayerNoSlider;
+    public Dropdown StrategyDrop;
+    public Dropdown ActionDrop;
+
+    // Use this for initialization
+    void Start () {
+        CreateButton.onClick.AddListener(delegate { CreateSimulation(); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateSimulation()
+    {
+        Configuration c = new Configuration();
+        c.Title = TitleInput.text;
+        c.Description = DescInput.text;
+        c.CreationDateTime = DateTime.Now;
+        c.ModifiedDateTime = DateTime.Now;
+        c.NumberOfRobots = (int) PlayerNoSlider.value;
+
+        Configuration.Serialize(Directory.GetCurrentDirectory() + "\\Saved\\" + c.Title + ".config", c);
+    }
+}
