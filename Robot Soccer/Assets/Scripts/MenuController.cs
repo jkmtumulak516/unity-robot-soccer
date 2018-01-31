426e6b0b55e3865eb252e79ef12f51dbc3299a07
Jhon Christian Ambrad <jambrad123@gmail.com>
2018-01-31 23:44:53 +0800

    stuff

--- Robot Soccer/Assets/Scripts/MenuController.cs
+++ Robot Soccer/Assets/Scripts/MenuController.cs
@@ -19,7 +19,7 @@ public class MenuController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        HideIfClickedOutside(SidePanel);
+        //HideIfClickedOutside(SidePanel);
 	}
 
     void TogglePanel()
