2e09bba34ea955358db0c907c212a756733598d5
Jhon Christian Ambrad <jambrad123@gmail.com>
2018-01-31 20:40:28 +0800

    Added Hamburger menu

---
+++ Robot Soccer/Assets/Scripts/MenuController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    // Use this for initialization
+    Animator SidePanelAnimator;
+    public GameObject SidePanel;
+    public Button HamburgerButton;
+    int PanelTriggerHash = Animator.StringToHash("TogglePanel");
+    int IdleOutSHash = Animator.StringToHash("Base Layer.IdleOut");
+
+	void Start () {
+        SidePanelAnimator = SidePanel.GetComponent<Animator>();
+        HamburgerButton.onClick.AddListener(delegate { TogglePanel(); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HideIfClickedOutside(SidePanel);
+	}
+
+    void TogglePanel()
+    {
+        SidePanelAnimator.SetTrigger(PanelTriggerHash);
+    }
+
+    private void HideIfClickedOutside(GameObject panel)
+    {
+        if (Input.GetMouseButton(0) && panel.activeSelf &&
+            !RectTransformUtility.RectangleContainsScreenPoint(
+                panel.GetComponent<RectTransform>(),
+                Input.mousePosition,
+                Camera.main))
+        {
+           if(SidePanelAnimator.GetCurrentAnimatorStateInfo(0).fullPathHash == IdleOutSHash)
+            {
+                TogglePanel();
+            }
+        }
+    }
+}
