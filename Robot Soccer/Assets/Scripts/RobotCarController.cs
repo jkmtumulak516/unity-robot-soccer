ecac1b034f9d05ec80d761e12cd8ea17d1dab834
Jhon Christian Ambrad <jambrad123@gmail.com>
2018-02-01 22:30:39 +0800

    Added goal line and soccer ball

--- Robot Soccer/Assets/Scripts/RobotCarController.cs
+++ Robot Soccer/Assets/Scripts/RobotCarController.cs
@@ -7,7 +7,8 @@ public class RobotCarController : MonoBehaviour {
 	public float maxMotorTorque; // maximum torque the motor can apply to wheel
 	public float leftMotorTorque;
 	public float rightMotorTorque;
-
+    public GameObject leftWheel;
+    public GameObject rightWheel;
 	// Use this for initialization
 	void Start () {
 		
@@ -21,7 +22,11 @@ public class RobotCarController : MonoBehaviour {
 			axleInfo.leftWheel.motorTorque = leftMotor;
 			axleInfo.rightWheel.motorTorque = rightMotor;
 		}
-	}
+
+        leftWheel.transform.Rotate(0,axleInfo.leftWheel.rpm / 60 * 360 * Time.deltaTime, 0);
+        rightWheel.transform.Rotate(0,axleInfo.rightWheel.rpm / 60 * 360 * Time.deltaTime, 0);
+
+    }
 
 	[System.Serializable]
 	public class AxleInfo {
