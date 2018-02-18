c23aa7497e4445b93bbf40ba3d85500be10a008d
Jhon Christian Ambrad <jambrad123@gmail.com>
2018-02-18 19:05:50 +0800

    Added League class

---
+++ Robot Soccer/Assets/Scripts/Game/League.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class League
+    {
+        public class Small
+        {
+            public static Vector3[] RobotPositions =  {
+                new Vector3(0, 2, 65.33f),
+                new Vector3(30.36f, 2, 14.04f),
+                new Vector3(0, 2, 48.7f)
+            };
+
+            public static Vector3 Scale = new Vector3(130f, 0.9f, 150f);
+            public static int CenterCirleRadius = 20;
+        }
+
+        public class Middle
+        {
+            public static Vector3[] RobotPositions = {
+                new Vector3(0, 2, 95f),
+                new Vector3(0, 2, 20f),
+                new Vector3(0, 2, 70f),
+                new Vector3(40, 2, 50f),
+                new Vector3(-40, 2, 50f),
+            };
+
+            public static Vector3 Scale = new Vector3(180f, 0.9f, 220f);
+            public static int CenterCirleRadius = 25;
+        }
+
+        public class Large
+        {
+            public static Vector3[] RobotPositions = {
+                new Vector3(0, 2, 175f),
+                new Vector3(0, 2, 30f),
+                new Vector3(50, 2, 30f),
+                new Vector3(-50, 2, 30f),
+                new Vector3(25, 2, 85f),
+                new Vector3(-25, 2, 85f),
+                new Vector3(75, 2, 85f),
+                new Vector3(-75, 2, 85f),
+                new Vector3(50, 2, 125f),
+                new Vector3(0, 2, 140f),
+                new Vector3(-50, 2, 125f),
+            };
+
+            public static Vector3 Scale = new Vector3(280f, 0.9f, 400f);
+            public static int CenterCirleRadius = 70;
+        }
+    }
+}
---
+++ Robot Soccer/Assets/Scripts/SelectSimulation/OnSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnSelect : MonoBehaviour {
+
+	// Use this for initialization
+	ConfigurationController ConfigController;
+
+	void Start () {
+		ConfigController = this.transform.parent.GetComponent<ConfigurationController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Select() {
+		Button[] b = this.transform.parent.GetComponentsInChildren<Button> (true);
+		foreach (Button bt in b) {
+			bt.interactable = true;
+		}
+		this.GetComponent<Button> ().interactable = false;
+		ConfigController.DetailsTrigger (this.transform.GetSiblingIndex ());
+	}
+}
