6e726c3c0e4c489c7bb871840b2e3729f4d0773b
Jhon Christian D. Ambrad <jambrad123@gmail.com>
2018-03-03 01:10:11 +0800

    Added GameStateFLS to GameStateController

--- Robot Soccer/Assets/Scripts/Game/GameStateController.cs
+++ Robot Soccer/Assets/Scripts/Game/GameStateController.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.Helper;
+﻿using Assets.Scripts.FuzzyLogic.GameState.FuzzySystems;
+using Assets.Scripts.Helper;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,8 +13,11 @@ public class GameStateController : MonoBehaviour {
     RobotCarController NearestBlue;
     RobotCarController NearestRed;
 
+    GameStateFLS GameStateFLS;
+
     // Use this for initialization
 	void Start () {
+        GameStateFLS = new GameStateFLS();
 	}
 	
 	// Update is called once per frame
@@ -21,34 +25,37 @@ public class GameStateController : MonoBehaviour {
         NearestBlue = BlueTeam.GetClosestFromBall();
         NearestRed = RedTeam.GetClosestFromBall();
     }
-
-    //TODO Intergrate GetState with StateFLS
-    int GetStateBlue()
+    
+    float GetStateBlue()
     {
         //Difference of the nearest distances between blue and red robots from ball
         var ND = ((NearestBlue.transform.position - Ball.transform.position).sqrMagnitude - (NearestRed.transform.position - Ball.transform.position).sqrMagnitude);
 
         //Orientation of the blue robot from the ball
-        var O = System.Math.Abs(Angle.ComputeRelativeAngle(NearestBlue.transform.eulerAngles.y, NearestBlue.transform.position.x, NearestBlue.transform.position.z, Ball.transform.position.x, Ball.transform.position.z));
+        var O = (float) System.Math.Abs(Angle.ComputeRelativeAngle(NearestBlue.transform.eulerAngles.y, NearestBlue.transform.position.x, NearestBlue.transform.position.z, Ball.transform.position.x, Ball.transform.position.z));
 
         //Calculated distance of the ball from the red goal
         var D = Vector3.Distance(Ball.transform.position, BlueTeam.OpponentGoal.transform.position);
-        return 1;
-    }
 
-    //TODO Intergrate GetState with StateFLS
-    int GetStateRed()
+        var state = GameStateFLS.GetState(O, ND, D);
+
+        return state;
+    }
+    
+    float GetStateRed()
     {
         //Difference of the nearest distances between red and blue robots from ball
         var ND = ((NearestRed.transform.position - Ball.transform.position).sqrMagnitude - (NearestBlue.transform.position - Ball.transform.position).sqrMagnitude);
 
         //Orientation of the red robot from the ball
-        var O = System.Math.Abs(Angle.ComputeRelativeAngle(NearestRed.transform.eulerAngles.y, NearestRed.transform.position.x, NearestRed.transform.position.z, Ball.transform.position.x, Ball.transform.position.z));
+        var O = (float) System.Math.Abs(Angle.ComputeRelativeAngle(NearestRed.transform.eulerAngles.y, NearestRed.transform.position.x, NearestRed.transform.position.z, Ball.transform.position.x, Ball.transform.position.z));
 
         //Calculated distance of the ball from the blue goal
         var D = Vector3.Distance(Ball.transform.position, RedTeam.OpponentGoal.transform.position);
 
-        return 1;
+        var state = GameStateFLS.GetState(O, ND, D);
+
+        return state;
     }
 
 }
