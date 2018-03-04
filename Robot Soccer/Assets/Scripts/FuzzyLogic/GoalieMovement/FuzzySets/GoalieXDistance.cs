4f8377b8c87ee98702e68bc8965dad22050da8be
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-03-05 00:52:28 +0800

    implemented concrete classes for the Goalie FuzzySets
    created FuzzySystem classes for Goalie

--- Robot Soccer/Assets/Scripts/FuzzyLogic/GoalieMovement/FuzzySets/GoalieXDistance.cs
+++ Robot Soccer/Assets/Scripts/FuzzyLogic/GoalieMovement/FuzzySets/GoalieXDistance.cs
@@ -6,7 +6,7 @@ namespace Assets.Scripts.FuzzyLogic.GoalieMovement.FuzzySets
     {
         protected IInputFuzzyMember _inside;
         protected IInputFuzzyMember _slightly_inside;
-        protected IInputFuzzyMember _on_line;
+        protected IInputFuzzyMember _on_goal_line;
         protected IInputFuzzyMember _slightly_outside;
         protected IInputFuzzyMember _moderately_outside;
         protected IInputFuzzyMember _far_outside;
@@ -16,7 +16,7 @@ namespace Assets.Scripts.FuzzyLogic.GoalieMovement.FuzzySets
 
         public IInputFuzzyMember Inside { get { return _inside; } }
         public IInputFuzzyMember SlightlyInside{ get { return _slightly_inside; } }
-        public IInputFuzzyMember OnLine { get { return _on_line; } }
+        public IInputFuzzyMember OnGoalLine { get { return _on_goal_line; } }
         public IInputFuzzyMember SlightlyOutside{ get { return _slightly_outside; } }
         public IInputFuzzyMember ModeratelyOutside { get { return _moderately_outside; } }
         public IInputFuzzyMember FarOutside { get { return _far_outside; } }
---
+++ Robot Soccer/Assets/Scripts/FuzzyLogic/GoalieMovement/FuzzySets/XDistances/SmallGoalieXDistance.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using FuzzyLogicSystems.Core.Values;
+using FuzzyLogicSystems.Core.Values.Generic;
+
+namespace Assets.Scripts.FuzzyLogic.GoalieMovement.FuzzySets.XDistances
+{
+    public class SmallGoalieXDistance : GoalieXDistance
+    {
+        public SmallGoalieXDistance(int category) : base(category) { }
+
+        protected override ICollection<IInputFuzzyMember> InitializeMembers()
+        {
+            var members = new HashSet<IInputFuzzyMember>();
+
+            _inside = new LinearInput("Inside", this, 0f, true, false, 2.5f, 0f);
+            _slightly_inside = new LinearInput("Slightly Inside", this, 2.5f, false, false, 2.5f, 0f);
+            _on_goal_line = new LinearInput("On Goal Line", this, 5f, false, false, 2.5f, 0f);
+            _slightly_outside = new LinearInput("Slightly Outside", this, 10f, false, false, 5f, 0f);
+            _moderately_outside = new LinearInput("Moderately Outside", this, 15f, false, false, 5f, 0f);
+            _far_outside = new LinearInput("Far Outside", this, 20f, false, false, 5f, 0f);
+            _very_far_outside = new LinearInput("Very Far Outside", this, 25f, false, true, 5f, 0f);
+
+            members.Add(_inside);
+            members.Add(_slightly_inside);
+            members.Add(_on_goal_line);
+            members.Add(_slightly_outside);
+            members.Add(_moderately_outside);
+            members.Add(_far_outside);
+            members.Add(_very_far_outside);
+
+            return members;
+        }
+    }
+}
