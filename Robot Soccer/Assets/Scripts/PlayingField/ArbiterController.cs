355ce68d32a237756f3a3df430a2c2082353ae6f
Jhon Christian D. Ambrad <jambrad123@gmail.com>
2018-03-06 05:13:23 +0800

    Move towards ball if not within sphere cast

--- Robot Soccer/Assets/Scripts/PlayingField/ArbiterController.cs
+++ Robot Soccer/Assets/Scripts/PlayingField/ArbiterController.cs
@@ -8,17 +8,35 @@ public class ArbiterController : MonoBehaviour {
     private ArbiterFLS ArbiterFls;
     public RobotCarController ArbiterRobot;
     public GameObject Ball;
+
+    public float RobotBallRadius = 10;
+    int BallLayer;
 	// Use this for initialization
 	void Start () {
         this.ArbiterFls = new ArbiterFLS();
-
+        BallLayer = LayerMask.GetMask("Ball");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         if(ArbiterRobot != null)
         {
-            //stuff
+            
+            if (Physics.OverlapSphere(this.transform.position, RobotBallRadius, 1 << BallLayer).Length == 1)
+            {
+
+            }else
+            {
+                //Move towards ball
+                ArbiterRobot.DestX = Ball.transform.position.x;
+                ArbiterRobot.DestY = Ball.transform.position.z;
+            }
         }
 	}
+
+    float GetPassStatus()
+    {
+
+        return 0f;
+    }
 }
